05834562a376bb01b06fc96bbe19f03b8cd7e8e0
Chris <cgm19@pitt.edu>
2015-05-27 01:05:05 +0000

    Initial Library and Driver setup

---
+++ graphicsDriver.c
@@ -0,0 +1,9 @@
+#include "library.h"
+#include <stdio.h>
+
+int main(void){
+
+init_graphics();
+exit_graphics();
+return 0;
+}
---
+++ library.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "library.h"
+
+void init_graphics(){
+printf("Init graphics");
+}
+
+void exit_graphics(){
+printf("exit graphics");
+}
