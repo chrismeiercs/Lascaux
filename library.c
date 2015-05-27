904c0793ed87a5d90d3fc0c59db38cce8e064759
Chris <cgm19@pitt.edu>
2015-05-27 01:29:49 +0000

    Open and close graphics device

--- library.c
+++ library.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include "library.h"
-
+int graphics_device;
 void init_graphics(){
 printf("Init graphics\n");
-printf("Open graphics device\n");
+printf("Opening graphics device\n");
+graphics_device = open("/dev/fb0",O_RDWR | O_APPEND);
+if(graphics_device >-1){
+printf("Graphics Device opened\n");
+}
 printf("mmap() memory\n");
 printf("Screen Info\n");
 printf("ioctl() syscall\n\n");
@@ -13,7 +19,12 @@ printf("ioctl() syscall\n\n");
 
 void exit_graphics(){
 printf("exit graphics\n");
-printf("Close graphics device\n");
+printf("Closing graphics device\n");
+if(close(graphics_device) == 0){
+printf("Graphics device closed\n");
+}
+
+
 printf("munmap() memory\n");
 printf("Renable keys with ioctl\n\n");
 }
