623238581075f8c6d1b4377dd60e32ea6dfd736b
Chris <cgm19@pitt.edu>
2015-05-27 01:17:49 +0000

    Init and Exit fx calls implemented

--- library.c
+++ library.c
@@ -2,9 +2,18 @@
 #include "library.h"
 
 void init_graphics(){
-printf("Init graphics");
+printf("Init graphics\n");
+printf("Open graphics device\n");
+printf("mmap() memory\n");
+printf("Screen Info\n");
+printf("ioctl() syscall\n\n");
+
+
 }
 
 void exit_graphics(){
-printf("exit graphics");
+printf("exit graphics\n");
+printf("Close graphics device\n");
+printf("munmap() memory\n");
+printf("Renable keys with ioctl\n\n");
 }
--- library.h
+++ library.h
@@ -1,3 +1,8 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+typedef unsigned short color_t;
+
 void init_graphics();
 void exit_graphics();
 void clear_screen();
@@ -6,3 +11,4 @@ void sleep_ms(long ms);
 void draw_pixel(int x, int y, color_t color);
 void draw_rect(int x1, int y1, int width, int height, color_t c);
 void draw_text(int x, int y, const char *text, color_t c);
+#endif
