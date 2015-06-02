4d385c473fbf8318010df4b7f7d7d78a1e464249
Chris <cgm19@pitt.edu>
2015-06-02 02:31:32 +0000

    Partial key input read

--- graphicsDriver.c
+++ graphicsDriver.c
@@ -8,13 +8,24 @@ printf("Graphics Initialized");
 
 }
 
-if(sleep_ms(10)== 0){
+/*if(sleep_ms(10)== 0){
 	printf("Sleep successful\n");
+}*/
+char keyPressed;
+/*do{
+keyPressed = getKey();
+if(keyPressed == 'a'){
+//printf("Key pressed: %c\n",keyPressed);
 }
+}
+while(keyPressed != 'q');
+*/
 
+while(1){
 
+getKey();
 
-
+}
 if(exit_graphics()){
 printf("Graphics Closed");
 
--- library.c
+++ library.c
@@ -1,3 +1,4 @@
+#include <sys/select.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -5,12 +6,16 @@
 #include <sys/mman.h>
 #include <time.h>
 #include <linux/fb.h>
+#include <termios.h>
 #include "library.h"
 
 int graphics_device;
 struct fb_var_screeninfo varInfo;
 struct fb_fix_screeninfo fixInfo;
 void *screen;
+fd_set fileSet;
+struct timeval timeOut;
+static struct termios oldt, newt;
 
 
 int init_graphics(){
@@ -48,6 +53,27 @@ return 1;
 
 printf("ioctl() syscall\n\n");
 
+/*
+printf("Remove Canonical Mode\n");
+
+tcgetattr(0,&oldt);
+newt = oldt;
+
+newt.c_lflag &= ~(ICANON);
+
+tcsetattr(0, TCSANOW, &newt);
+*/
+
+printf("Add set select listening\n");
+FD_ZERO(&fileSet);
+FD_SET(0,&fileSet);
+
+printf("Set select timeout\n");
+timeOut.tv_sec = 0;
+timeOut.tv_usec = 0;
+
+
+
 return 0;
 }
 
@@ -69,10 +95,47 @@ if(munmap(screen, varInfo.yres_virtual * fixInfo.line_length) == -1){
 
 printf("Renable keys with ioctl\n\n");
 
+printf("Set input listening\n");
+
+FD_ZERO(&fileSet);
+
+/*
+printf("Restore cannonical mode");
+
+tcsetattr(0, TCSANOW, &oldt);
+*/
+
 return 0;
 }
 
-int sleep_ms(long ms){
+/*int sleep_ms(long ms){
 	//need struct timespec w/ tv_nsec field (long)
 	return nanosleep(req, NULL);
+}*/
+
+void getKey(){
+
+FD_ZERO(&fileSet);
+FD_SET(0,&fileSet);
+/*int selRes = select(1,&fileSet,NULL,NULL,&timeOut);
+
+
+if(selRes){
+printf("select value: %d\n",selRes);
+}*/
+
+//input = 0
+
+if(select(1,&fileSet,NULL,NULL, &timeOut) == 1){
+	char key[1];
+	/*if(read(0,key,1)==1){
+		printf("Key pressed: %c\n", key);
+		//return key;
+	}*/
+	
+	if(read(0,&key,1)){
+	printf("Key pressed %c\n", key[0]);
+	}
+}
+
 }
